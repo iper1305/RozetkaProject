bbadd9f9e598bc91171a6e31c1c8bb38a07b0d37
Irina Perepada <86161724+iper1305@users.noreply.github.com>
2021-12-16 10:01:52 +0200

    Update RozetkaTestWithPO.java

--- src/test/java/RozetkaTestWithPO.java
+++ src/test/java/RozetkaTestWithPO.java
@@ -1,4 +1,3 @@
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
